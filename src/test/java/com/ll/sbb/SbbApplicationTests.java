791fcbfa5c0d85c57bc891f4c5e7857e77430408
J-Cheol <lunaris0206@naver.com>
2024-01-04 23:37:41 +0900

    findById

--- src/test/java/com/ll/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/sbb/SbbApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,12 +18,13 @@ class SbbApplicationTests
     private QuestionRepository questionRepository;
 
     @Test
-    void testJpa() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    void testJpa()
+    {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if (oq.isPresent())
+        {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
     }
-
 }
